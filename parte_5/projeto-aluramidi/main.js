0f227935ffc0f71b5769f9cf9305ca4d396a9ab4
Lucas Caminha <lcaminhaslz@gmail.com>
2023-03-16 15:47:48 -0300

    Adicionando conteúdo da unidade 5.2.4

--- parte_5/projeto-aluramidi/main.js
+++ parte_5/projeto-aluramidi/main.js
@@ -2,8 +2,12 @@
 // Função teste de link <script>:
 // alert('Olá mundo!');
 
-function tocaSomPom() {
-  document.querySelector('#som_tecla_pom').play();
+// function tocaSomPom() {
+//   document.querySelector('#som_tecla_pom').play();
+// }
+
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 // document.querySelector('.tecla_pom').onclick = tocaSomPom;
@@ -18,4 +22,44 @@ document.querySelector('.tecla_clap').onclick = tocaSomClap;*/
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+// listaDeTeclas[0].onclick = tocaSomPom;
+
+//let contador = 0;
+
+//Enquanto isso...
+/*while (contador < listaDeTeclas.length) {
+  // listaDeTeclas[contador].onclick = tocaSomPom;
+  // listaDeTeclas[contador].onclick = tocaSom;
+  // listaDeTeclas[contador].onclick = function () { //função anônima (uma função sem nome temporária)
+  //   tocaSom('#som_tecla_pom');
+  // }
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  // console.log(instrumento);
+  
+  //template string
+  const idAudio = `#som_${instrumento}`; //utilização da craze " ` "
+  // console.log(idAudio);
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+
+  // contador += 1;
+  contador++;
+  // console.log(contador);
+}*/
+
+//Para isso...
+for (contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  
+  const idAudio = `#som_${instrumento}`; //template string
+  // console.log(idAudio);
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+}
